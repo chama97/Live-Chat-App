4c0d5d3fb3e50516fcdbfdafbbf8c3fa9b70b2de
chama97 <chamamanchanayake5@gmail.com>
2022-08-12 16:04:41 +0530

    add server threads

---
+++ src/ServerSide/ServerThread.java
@@ -0,0 +1,45 @@
+package ServerSide;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerThread extends Thread{
+    private Socket socket;
+    private ArrayList<ServerThread> threadArrayList;
+    private PrintWriter output;
+
+    public ServerThread(Socket socket, ArrayList<ServerThread> threads){
+        this.socket = socket;
+        this.threadArrayList = threads;
+    }
+
+    @Override
+    public void run(){
+        try{
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            output = new PrintWriter(socket.getOutputStream(), true);
+
+            while(true){
+                String outputString = input.readLine();
+
+                if(outputString.equals("end")){
+                    break;
+                }
+                printToAllClients(outputString);
+                System.out.println("Server received " + outputString);
+            }
+        } catch (Exception e){
+            System.out.println("Error occurred " + e.getStackTrace());
+        }
+    }
+
+    private void printToAllClients(String outputString) {
+        for (ServerThread sT : threadArrayList) {
+            sT.output.println(outputString);
+        }
+    }
+}
